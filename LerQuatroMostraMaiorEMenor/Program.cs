26f9782f1417b947138cfe28d591728e473d7e84
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 20:05:43 -0300

    Add project files.

---
+++ LerQuatroMostraMaiorEMenor/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int[4];
+int maior, menor;
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Digite um numero: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+maior = numbers.Max();
+menor = numbers.Min();
+Console.Write("Maior número digitado: {0}\n" +
+    "Menor número digitado: {1}", maior, menor);
